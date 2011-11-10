f538ce299d3424b1ed6a6c14ce5d83e81a76464d
alexgl <nemofulcrum@gmail.com>
2011-11-10 10:23:26 -0800

    work in progress, adding branches to the main git, next adding tags

--- tripal_svn_git_migration.sh
+++ tripal_svn_git_migration.sh
@@ -17,10 +17,18 @@ mkdir tripal_branches
 git svn init https://gmod.svn.sourceforge.net/svnroot/gmod/tripal/branches --no-metadata
 git config svn.authorsfile ../../users.txt
 git svn fetch
+git checkout -b to_merge_tripal
+git mv -r tripal-7.x-trunk/* ./
+rm -r tripal-7.x-trunk
+git add .
+git commit -am'commit to merge branch'
 
-#cd ../tripal
-#git remote add tripal-7.x-trunk git:../tripal_branches/tripal-7.x-trunk 
-#
-#git fetch rweb
+
+cd ../tripal
+git remote add tripal_d7_repo git:../tripal_branches/ 
+git fetch tripal_d7_repo
+git branch tripal_d7 tripal_d7_repo/to_merge_tripal
+
+#git checkout -b 
 
 
