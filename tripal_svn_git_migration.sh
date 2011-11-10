f2f42480558b5ccb4a90cf25f2463dfeb2d80959
alexgl <nemofulcrum@gmail.com>
2011-11-09 17:39:18 -0800

    getting branches tags and trunk

--- tripal_svn_git_migration.sh
+++ tripal_svn_git_migration.sh
@@ -6,3 +6,21 @@ git svn init https://gmod.svn.sourceforge.net/svnroot/gmod/tripal/trunk --no-met
 git config svn.authorsfile ../../users.txt
 git svn fetch
 
+cd ../
+mkdir tripal_tags
+git svn init https://gmod.svn.sourceforge.net/svnroot/gmod/tripal/tags --no-metadata
+git config svn.authorsfile ../../users.txt
+git svn fetch
+
+cd ../
+mkdir tripal_branches
+git svn init https://gmod.svn.sourceforge.net/svnroot/gmod/tripal/branches --no-metadata
+git config svn.authorsfile ../../users.txt
+git svn fetch
+
+#cd ../tripal
+#git remote add tripal-7.x-trunk git:../tripal_branches/tripal-7.x-trunk 
+#
+#git fetch rweb
+
+
